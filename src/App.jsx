8cf5fcb5c08e63422ac6254d048255d9181dfcf9
Emirhan Gözüküçük <emirhan_g7@outlook.com>
2023-12-15 19:58:07 +0300

    v2.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,17 @@ import { useState } from "react"
 import Subscribe from "./assets/components/Subscribe"
 import Subscribing from "./assets/components/Subscribing"
 
-
 export default function App() {
   const [isFormSubmitted, setFormSubmit] = useState(false)
   const [email, setEmail] = useState("")
   
-
   return (
     <>
       <div className="container">
         {
           isFormSubmitted
           ?
-          <Subscribing email={email} />
+          <Subscribing setFormSubmit={setFormSubmit} email={email} />
           :
           
             <div className="hero">
@@ -24,7 +22,8 @@ export default function App() {
                 <Subscribe email={email} setEmail={setEmail} isFormSubmitted={isFormSubmitted} setFormSubmit={setFormSubmit}/>
               </div>
               <div className="bgImg">
-                <img src="./src/assets/Img/mainPageImg.png" alt="Main Page Img" />
+                <img className="desktop" src="./src/assets/Img/mainPageImg.png" alt="Main Page Img" />
+                <img className="mobile" src="./src/assets/Img/mainPageImgMobile.jpg" alt="Main Page Img" />
               </div>
             </div>
         }
